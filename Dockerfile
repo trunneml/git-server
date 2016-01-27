c3c12e7e0a1d40fe6c40f0f6f516b7c8a7223929
Michael Trunner <michael@trunner.de>
2016-01-27 22:05:56 +0100

    Improved Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,16 +8,19 @@ RUN dnf install -y \
         passwd \
         ; dnf clean all
 
+
 ENV GIT_HOME /srv/git
-RUN mkdir /srv/git
+RUN mkdir -p $GIT_HOME
+RUN useradd -M -d $GIT_HOME -U git
+
+CMD ["/usr/sbin/sshd", "-D"]
+
+EXPOSE 2222
 VOLUME /srv/git
 
-RUN useradd -M -d /srv/git -U git
+COPY sshd_config /etc/ssh/sshd_config
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod 755 /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
-CMD ["/usr/sbin/sshd", "-D"]
-
-EXPOSE 22
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,19 @@ __configure_user() {
   echo git user password: $GIT_USERPASS
   mkdir -p $GIT_HOME/.ssh
   chown git:git -R $GIT_HOME/.ssh
-  chmod 700 -R $GIT_HOME/.ssh
-  if [ -n "${GIT_KEYS}" ] && [ ! -f $GIT_HOME/.ssh/authorized_keys ]; then
-    curl -o $GIT_HOME/.ssh/authorized_keys $GIT_KEYS
+  chmod o-rwx -R $GIT_HOME/.ssh
+  chown git:git $GIT_HOME
+  chmod 700 $GIT_HOME
+  if [ -n $GIT_KEYS ]; then
+    curl -L -o $GIT_HOME/.ssh/authorized_keys2 $GIT_KEYS
   fi
 }
 
 __create_hostkeys() {
-  ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
+  install -g root -o root -m 770 -d ${GIT_HOME}/.sshd
+  test -f ${GIT_HOME}/.sshd/ssh_host_rsa_key || ssh-keygen -t rsa -f ${GIT_HOME}/.sshd/ssh_host_rsa_key -N ''
+  test -f ${GIT_HOME}/.sshd/ssh_host_ecdsa_key || ssh-keygen -t ecdsa -f ${GIT_HOME}/.sshd/ssh_host_ecdsa_key -N ''
+  test -f ${GIT_HOME}/.sshd/ssh_host_ed25519_key || ssh-keygen -t ed25519 -f ${GIT_HOME}/.sshd/ssh_host_ed25519_key -N ''
 }
 
 # Call all functions
